8ea1158a38e8d950528baa55225d7a6597e18850
coinpower Italy <coinpoweritaly@gmail.com>
2026-03-16 06:39:21 +0000

    const firebaseConfig = {   apiKey: "AIzaSyC6Ic0c7UeDJcwgDTHo9JwnpjmCSWPL

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4298733968-6d0dd",
-  "appId": "1:946021355451:web:742d767d414371d30ebe9e",
-  "apiKey": "AIzaSyC6Ic0c7UeDJcwgDTHo9JwnpjmCSWPLdAc",
-  "authDomain": "studio-4298733968-6d0dd.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "946021355451"
+  apiKey: "AIzaSyC6Ic0c7UeDJcwgDTHo9JwnpjmCSWPLdAc",
+  authDomain: "studio-4298733968-6d0dd.firebaseapp.com",
+  projectId: "studio-4298733968-6d0dd",
+  storageBucket: "studio-4298733968-6d0dd.appspot.com",
+  messagingSenderId: "946021355451",
+  appId: "1:946021355451:web:742d767d414371d30ebe9e"
 };
