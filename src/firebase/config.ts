5060b7c86daa6ab96aa62881e3ee4b56c26092bc
coinpower Italy <coinpoweritaly@gmail.com>
2026-03-16 05:02:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4298733968-6d0dd",
+  "appId": "1:946021355451:web:742d767d414371d30ebe9e",
+  "apiKey": "AIzaSyC6Ic0c7UeDJcwgDTHo9JwnpjmCSWPLdAc",
+  "authDomain": "studio-4298733968-6d0dd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "946021355451"
+};
