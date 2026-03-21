8a37f2fdb7b84c990c0cb78707f18a8e01b43339
coinpower Italy <coinpoweritaly@gmail.com>
2026-03-21 08:35:42 +0000

    const config = {   default: {     override: {       wrapper: "cloudflare

---
+++ src/open-next.config.ts
@@ -0,0 +1,22 @@
+import { type OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "async-local-storage",
+      tagCache: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+};
+
+export default config;
