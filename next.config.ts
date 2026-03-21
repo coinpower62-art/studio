5cd12b8c49341bbbe8124c8967e5c7bc5a4a2602
coinpower Italy <coinpoweritaly@gmail.com>
2026-03-21 17:03:06 +0000

    Fix this conflict

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+  allowedDevOrigins: [
+    "https://6000-firebase-studio-1773587620136.cluster-v6vfua76y5hvquszcbrvi5d4oi.cloudworkstations.dev",
+    "http://6000-firebase-studio-1773587620136.cluster-v6vfua76y5hvquszcbrvi5d4oi.cloudworkstations.dev"
+  ]
+};
+
+export default nextConfig;
