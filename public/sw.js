16855daa245837e10c83404174255b64c0dcd71b
Your Name <you@example.com>
2026-03-29 17:37:03 +0000

    self.addEventListener('install', (e) => self.skipWaiting()); self.addEve

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,2 @@
-// This is a basic service worker file.
-// It's required for a web app to be installable (PWA).
-self.addEventListener('install', (event) => {
-  console.log('Service worker installed');
-});
-
-self.addEventListener('fetch', (event) => {
-  // A basic fetch handler.
-  // For a more robust offline experience, you would add caching strategies here.
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
