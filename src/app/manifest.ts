5b39d946df660cc5318435c4d89afbb0e24fe6c4
coinpower Italy <coinpoweritaly@gmail.com>
2026-03-29 14:44:03 +0000

    // app/manifest.ts import { MetadataRoute } from 'next'
    export default

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,18 +4,18 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'CoinPower Digital Mining',
     short_name: 'CoinPower',
-    description: 'Mining energy on the go',
-    start_url: 'https://coinpower-app.vercel.app/',
-    id: 'https://coinpower-app.vercel.app/',
     display: 'standalone',
     background_color: '#FFFFFF',
     theme_color: '#D4AF37',
+    description: 'Mining energy on the go',
+    start_url: 'https://coinpower-app.vercel.app/',
+    id: 'https://coinpower-app.vercel.app/',
     icons: [
       {
         src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any maskable', 
       },
       {
         src: '/icon-512.png',
